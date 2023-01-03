55700928b253605462752284dfac2318ed67c56b
Alfirman Ejha Pahlepi <alfirman.pahlepi@gmail.com>
2023-01-03 09:21:54 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+![Preview](https://firebasestorage.googleapis.com/v0/b/image-storage-aaa6b.appspot.com/o/Alfirman%20Pahlepi.jpg?alt=media&token=bb5ffed4-bc53-48a9-b3ea-ab32e4397c48)
---
+++ js/active.js
@@ -0,0 +1,10 @@
+(function ($) {
+"use strict";
+/*--------------------------------------
+	Counter Up Active
+----------------------------------------*/
+$('.counter').counterUp({
+	delay:10,
+	time:2000,
+});
+})(jQuery);	
